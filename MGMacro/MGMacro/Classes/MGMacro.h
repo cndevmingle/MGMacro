8273efc5a797cf816236381c9a879b65e2b2a451
Mingle <cndevmingle@gmail.com>
2018-04-09 15:22:29 +0800

    修改名称

--- MGMacro/MGMacro/Classes/MGMacro.h
+++ MGMacro/MGMacro/Classes/MGMacro.h
@@ -17,7 +17,7 @@
 #define MGNotificationCenter [NSNotificationCenter defaultCenter]
 
 // 颜色
-#define MGSCREEN_RandomColor [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1.0]
+#define MGRandomColor [UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0 blue:arc4random_uniform(256)/255.0 alpha:1.0]
 #define MGRGBColor(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
 #define MGRGBAColor(r, g, b, a) [UIColor colorWithRed:(r)/255.0 green:(r)/255.0 blue:(r)/255.0 alpha:a]
 #define MGColorWithAHex(rgbValue, a) \
